099f0f127e2586beca0194f79ddc0462e3f1128f
Emil Larsson <emil@jacero.se>
2019-04-23 21:53:22 +0200

    Auto stash before merge of "master" and "origin/master"

--- .flaskenv
+++ .flaskenv
@@ -1,7 +1,7 @@
 FLASK_APP=rest_api
 FLASK_ENV=development
 FLASK_DEBUG=1
-POSTGRES_URL="127.0.0.1:5432"
+POSTGRES_URL="db:5432"
 POSTGRES_USER="postgres"
 POSTGRES_PW="postgres"
 POSTGRES_DB="postgres"
--- app/routes.py
+++
@@ -1,6 +0,0 @@
-from app import api
-from rest_api import Release
-
-#api.add_resource(HelloWorld, '/hello', '/world')
-#api.add_resource(Todo, '/todo/<int:todo_id>', endpoint='todo_ep')
-api.add_resource(Release, '/api/v1/release/')
