5f3a78433bec9e2046bc833bac2d9e650dbb51ff
Emil Larsson <emil.larsson@zetup.se>
2019-04-23 22:03:46 +0200

    More stuff

--- app/seed_db.py
+++ app/seed_db.py
@@ -2,13 +2,49 @@ from app import db
 from app.models import ImageModel, ReleaseModel, DistroModel
 from datetime import datetime
 
-distro1 = DistroModel(name="ubuntu", company="Canonical", datetime_added=datetime.utcnow(), datetime_modified=datetime.utcnow())
-distro2 = DistroModel(name="centos", company="RedHat", datetime_added=datetime.utcnow(), datetime_modified=datetime.utcnow())
+distro1 = dict()
+distro1['name'] = "ubuntu"
+distro1['company'] = "Canonical"
+distro1['datetime_added'] = datetime.utcnow()
+distro1['datetime_modified'] = datetime.utcnow()
 
-release1 = ReleaseModel(name="16.04", datetime_added=datetime.utcnow(), datetime_modified=datetime.utcnow(), distro_id=1)
-release2 = ReleaseModel(name="18.04", datetime_added=datetime.utcnow(), datetime_modified=datetime.utcnow(), distro_id=1)
-release3 = ReleaseModel(name="6", datetime_added=datetime.utcnow(), datetime_modified=datetime.utcnow(), distro_id=2)
-release4 = ReleaseModel(name="7", datetime_added=datetime.utcnow(), datetime_modified=datetime.utcnow(), distro_id=2)
+distro2 = dict()
+distro2['name'] = "centos"
+distro2['company'] = "RedHat"
+distro2['datetime_added'] = datetime.utcnow()
+distro2['datetime_modified'] = datetime.utcnow()
 
-db.session.add_all([distro1, distro2, release1, release2, release3, release4 ])
+
+release1 = dict()
+release1['name'] = "16.04"
+release1['datetime_added'] = datetime.utcnow()
+release1['datetime_modified'] = datetime.utcnow()
+release1['distro_id'] = 1
+
+release2 = dict()
+release2['name'] = "18.04"
+release2['datetime_added'] = datetime.utcnow()
+release2['datetime_modified'] = datetime.utcnow()
+release2['distro_id'] =1
+
+release3 = dict()
+release3['name'] = "6"
+release3['datetime_added'] = datetime.utcnow()
+release3['datetime_modified'] = datetime.utcnow()
+release3['distro_id'] = 2
+
+release4 = dict()
+release4['name'] = "7"
+release4['datetime_added'] = datetime.utcnow()
+release4['datetime_modified'] = datetime.utcnow()
+release4['distro_id'] = 2
+
+
+objects = [DistroModel(distro1),
+           DistroModel(distro2),
+           ReleaseModel(release1),
+           ReleaseModel(release2),
+           ReleaseModel(release3),
+           ReleaseModel(release4)]
+db.session.bulk_save_objects(objects)
 db.session.commit()
--- rest_api.py
+++ rest_api.py
@@ -19,10 +19,10 @@ image_schema = ImageSchema()
 class Release(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('release_id', type=int, required=True)
+        parser.add_argument('id', type=int, required=True)
         args = parser.parse_args()
 
-        release = ReleaseModel.query.get(args['release_id'])
+        release = ReleaseModel.query.get(args['id'])
         ser_release = release_schema.dump(release).data
         return jsonify(ser_release)
 
@@ -37,12 +37,24 @@ class Releases(Resource):
 class Image(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('image_id', type=int, required=True)
+        parser.add_argument('id', type=int, required=True)
         args = parser.parse_args()
 
-        image = ImageModel.query.get(args['image_id'])
+        image = ImageModel.query.get(args['id'])
         ser_image = image_schema.dump(image).data
         return jsonify(ser_image)
+
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        args = parser.parse_args()
+
+        image = ImageModel.query.get(args['id'])
+        image.delete()
+        result = dict()
+        result['Message'] = {}
+        result['Message'] = "Image deleted [id: {}, name: {}]".format(image.id, image.name)
+        return jsonify(result)
 
 
 class Images(Resource):
@@ -72,7 +84,7 @@ class ImageScheduleUpdate(Resource):
             download_image(image, image_temporary_path)
 
             # Update database
-            release = db.session.query(ReleaseModel).filter_by(release=args['release']).first()
+            release = db.session.query(ReleaseModel).filter_by(name=args['release']).first()
 
             data = dict()
             data['build'] = image.build,
@@ -97,30 +109,31 @@ class ImageScheduleUpdate(Resource):
 
 class ImagesScheduleUpdate(Resource):
     def post(self):
-        result = dict()
+        result = []
+        image_db_data = []
         # Find and download latest release
         query = db.session.query(ReleaseModel, DistroModel).filter(ReleaseModel.distro_id == DistroModel.id).all()
         for release in query:
             release_name = release.ReleaseModel.name
             distro_name = release.DistroModel.name
-            print("TEST: {}-{}".format(release_name, distro_name))
 
             image = get_latest_release(release=release_name, distro=distro_name)
             release_distro_combo = "{}_{}".format(release_name, distro_name)
 
             # Check if image already exist
+            message = {}
             if ImageModel.image_exists(image.sha256):
-                result[release_distro_combo] = {}
-                result[release_distro_combo]['Message'] = {}
-                result[release_distro_combo]['Message'] = "The image already exist ({}-{})".format(image.name, image.build)
+                message[release_distro_combo] = {}
+                message[release_distro_combo]['Message'] = {}
+                message[release_distro_combo]['Message'] = "The image already exist ({}-{})".format(image.name, image.build)
+                result.append(message)
                 print("The image already exist ({})".format(image.sha256))
             else:
-                result = dict()
                 # Download image
                 download_image(image, image_temporary_path)
 
                 # Update database
-                release = db.session.query(ReleaseModel).filter_by(release=release_name).first()
+                release = db.session.query(ReleaseModel).filter_by(name=release_name).first()
 
                 data = dict()
                 data['build'] = image.build,
@@ -137,8 +150,9 @@ class ImagesScheduleUpdate(Resource):
 
                 db_image = ImageModel(data)
                 db_image.save()
-
-                result = image_schema.dump(db_image).data
+                #image_db_data.append(db_image)
+                #result = image_schema.dump(image_db_data, many=True).data
+                result.append(image_schema.dump(db_image).data)
 
         return jsonify(result)
 
@@ -160,119 +174,6 @@ def run_celery():
     return jsonify(result.result)
 
 
-@app.route('/api/releases', methods=['GET'])
-def get_releases():
-    all_releases = ReleaseModel.get_all_releases()
-    ser_releases = release_schema.dump(all_releases, many=True).data
-    return jsonify(ser_releases)
-
-
-@app.route('/api/images', methods=['GET'])
-def get_images():
-    all_images = ImageModel.get_all_images()
-    ser_images = image_schema.dump(all_images, many=True).data
-    return jsonify(ser_images)
-
-
-
-
-
-## This initates a full refresh of all stored releases
-@app.route('/api/update_all_images', methods=['GET'])
-def update_all_images():
-    # Get all releases from database and store in variable
-    releases = ReleaseModel.get_all_releases()
-    ser_releases = release_schema.dump(releases, many=True).data
-
-    # Search each release (get url)
-    latest_releases_list = []
-    for release in releases:
-        latest = get_latest_release(distro=release.distro,
-                           release=release.release)
-        # returns: build, name, sha256, suffix, url
-        latest_releases_list.append(latest)
-
-    # Compare each release with existing in database
-
-    # Download all releases that are new
-    result = []
-    downloaded = {}
-    for latest_release in latest_releases_list:
-        image_uuid = uuid.uuid4()
-        print(latest_release['url'])
-        image = download_image(url=latest_release['url'], destination=root_path, uuid=image_uuid)
-        downloaded['uuid'] = uuid
-        downloaded['temp_path'] = image['path']
-        downloaded['build'] = latest_release['build']
-        downloaded['name'] = latest_release['name']
-        downloaded['sha256'] = latest_release['sha256']
-        downloaded['suffix'] = latest_release['suffix']
-        downloaded['url'] = latest_release['url']
-        result.append(downloaded)
-
-    return jsonify(result)
-
-
-## This
-@app.route('/api/v1.0/latest_release', methods=['GET'])
-def latest_release():
-    parser = reqparse.RequestParser()
-    parser.add_argument('distro', type=str, required=True)
-    parser.add_argument('release', type=str, required=True)
-    args = parser.parse_args()
-    distro = args['distro']
-    release = args['release']
-
-    data = crawler.get_latest_release(distro, release)
-    image_name = data['name']
-    image_sha256 = data['sha256']
-    image_url = data['url']
-    image_build = data['build']
-    image_suffix = data['suffix']
-
-    return jsonify(data)
-
-@app.route('/api/image', methods=['POST'])
-def add_image():
-    # Recieve input data
-    distribution_id = 1
-    release_id = 1
-    datetime_added = datetime.now()
-    datetime_changed = datetime.now()
-    build = request.json['build']
-    uuid = request.json['uuid']
-    name = request.json['name']
-    sha256 = request.json['sha256']
-    archive_path = request.json['archive_path']
-    archive_filename = request.json['archive_filename']
-    file_suffix = request.json['file_suffix']
-    url = request.json['url']
-
-    new_image = ImageModel(distribution_id,
-                      release_id,
-                      datetime_added,
-                      datetime_changed,
-                      build,
-                      uuid,
-                      name,
-                      sha256,
-                      archive_path,
-                      archive_filename,
-                      file_suffix,
-                      url)
-
-    # Validate that input is correct
-
-    # Validate that data not already exist
-
-    # Store data
-
-    # Retrieve stored data and return to caller
-
-    db.session.add(new_image)
-    db.session.commit()
-
-    return new_image
 """
 
 if __name__ == '__main__':
